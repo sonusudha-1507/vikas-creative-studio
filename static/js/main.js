37cfde49112d20a3da72d0fec694eeb3d0df3c2b
sonusudha-1507 <gracyheleneda@gmail.com>
2026-02-07 19:05:24 +0530

    Add cinematic portfolio preview section

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,14 @@
 // Global JS placeholder
 console.log("VIKAS frontend loaded");
+// Portfolio reveal
+gsap.from(".portfolio-item", {
+    scrollTrigger: {
+        trigger: ".portfolio-preview",
+        start: "top 80%",
+    },
+    y: 30,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.15,
+    ease: "power2.out"
+});
